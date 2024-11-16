ed3140bba58b8c2abd714351f72c237b5642cbb7
Carl Cortright <ckcortright@gmail.com>
2024-11-16 15:35:19 -0800

    fix build

--- landing/tailwind.config.js
+++ landing/tailwind.config.js
@@ -1,16 +1,12 @@
-export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {},
     },
-  },
-  plugins: [],
-};
+    plugins: [],
+}
