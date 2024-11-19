71513b824c9c8854786b656ee5acb941593351aa
Terence Bezman <terencebezman@gmail.com>
2024-11-18 21:24:21 -0500

    check in

--- app/hardhat.config.js
+++ app/hardhat.config.js
@@ -1,7 +1,10 @@
+const path = require("path");
+
 module.exports = {
   solidity: "0.8.0",
   paths: {
     sources: "./contracts/src", // Specify your contracts directory
+    libraries: path.join(__dirname, "lib"), // Add this line to include Foundry libraries
   },
   typechain: {
     outDir: "types",
