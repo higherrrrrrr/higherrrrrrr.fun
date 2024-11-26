04c3efd86d30ffd1182920374db59a0667159de1
Carl Cortright <ckcortright@gmail.com>
2024-11-25 16:05:15 -0800

    update launch date

--- app/src/constants.ts
+++ app/src/constants.ts
@@ -1,4 +1,4 @@
-export const LAUNCH_DATE = new Date("2024-11-26T23:59:59-05:00");
+export const LAUNCH_DATE = new Date("2024-11-26T12:00:00-05:00");
 
 export function isBeforeLaunch() {
   return new Date().getTime() < LAUNCH_DATE.getTime();
