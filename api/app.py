460d023dd59a3aefc4cbda5d16599733e9ab92b0
Carl Cortright <ckcortright@gmail.com>
2024-11-26 05:01:59 -0800

    api all working

--- api/app.py
+++ api/app.py
@@ -49,8 +49,9 @@ def create_app():
 
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
     # Use PORT environment variable provided by Cloud Run
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
