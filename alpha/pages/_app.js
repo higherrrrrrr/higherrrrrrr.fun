64e5162452de128465cf7e2b78326a76bff2b574
Carl Cortright <ckcortright@gmail.com>
2024-12-11 13:57:46 -0800

    integrate posthog

--- alpha/pages/_app.js
+++ alpha/pages/_app.js
@@ -3,10 +3,38 @@ import { Web3Provider } from '../components/Web3Provider';
 import MainLayout from '../layouts/MainLayout';
 import '../styles/globals.css';
 import Launch from './launch';
+import { useEffect } from 'react';
+import posthog from 'posthog-js';
+import { useRouter } from 'next/router';
 
 const queryClient = new QueryClient();
 
+// PostHog initialization
+if (typeof window !== 'undefined') {
+  posthog.init('phc_QObbSAeS9Bc3rBhOtDD0M5JUp5RDmPDQZVsmNQVXnFp', {
+    api_host: 'https://us.i.posthog.com',
+    person_profiles: 'always',
+  });
+}
+
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+  
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => {
+      if (process.env.NODE_ENV === 'production') {
+        posthog.capture('$pageview');
+      }
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   // If we're on the index page, show Launch instead
   const ShowComponent = Component.name === 'TokensList' ? Launch : Component;
 
